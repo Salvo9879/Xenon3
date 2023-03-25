b4671208b19ee74ab591aabbba8c8db1173e7a02
salvatorelapaglia@icloud.com <salvatorelapaglia@icloud.com>
2023-03-25 18:16:37 +0000

    Initial commit

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 
 # Import internal modules
-from package import ServerSettings, Setup
+from package import ServerSettings, Setup, db
 
 # Import external modules
 from flask import Flask
 
 # Variables
 app = Flask(__name__)
-ss = ServerSettings()
-su = Setup(app, ss)
+ss = ServerSettings(app)
+su = Setup(ss)
 
 # Initialization
 ss.init()
+db.init_app(app)
 
 if __name__ == '__main__':
     su.run()
