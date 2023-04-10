8fdad9b474715651d3526c2d7c27a9c8971f926d
salvatorelapaglia@icloud.com <salvatorelapaglia@icloud.com>
2023-04-10 23:18:56 +0100

    Cleaned the project ready for the branch.

--- package/server.py
+++ package/server.py
@@ -2,7 +2,6 @@
 # Import internal modules
 from package.databases import db
 from package.config import AppSettings
-from package.application_handler import ApplicationManager
 
 import package.package_handler as ph
 
@@ -12,8 +11,6 @@ from flask import Flask
 # Variables
 app = Flask(__name__)
 settings = AppSettings()
-applications_manager = ApplicationManager()
-
 # Application configuration
 app.config['SECRET_KEY'] = settings.secret_key
 app.config['SQLALCHEMY_DATABASE_URI'] = ph.get_db_paths()['users']
